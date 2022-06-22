e987bbca19ff73245bec6060c58c6f6d8c41bbd9
selcukemreozer <selcuk.emre.ozer@outlook.com>
2022-06-22 22:39:40 +0300

    added numbers and some settings

--- theClock.py
+++ theClock.py
@@ -2,14 +2,26 @@ import pygame
 import math
 from time import sleep
 
-donus_x = False
-donus_y = False
+sleep_sayac = 0.1
 derece = 360
 dereceDK = 360
 dereceST = 360
 sayac = 0
 
+sayi = 12
+
+beyaz=(255,255,255)
+mavi=(0,0,255)
+yesil=(34,139,34)
+turuncu=(255,69,0)
+kırmızı=(255,0,0)
+
 pygame.init()
+font = pygame.font.SysFont("ComicSansMs", 18)
+
+def sayi(value):
+    sayilar = font.render(str(int(value / 30)), 2, beyaz)
+    return  sayilar
 
 """def CIRCLE_DRAWER(x1, y1, x2, y2):
     pass"""
@@ -18,19 +30,20 @@ screen = pygame.display.set_mode([500, 500])
 run = True
 
 while run:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    screen.fill((255, 0, 0))
+    screen.fill((0, 0, 0))
 
     # pygame.draw.line(screen, (255, 255, 255), (x1, y1), (x2, y2), 4)
     xsaniye = -50 * round(math.sin(math.radians(derece)), 5) + 250
     ysaniye = -50 * round(math.cos(math.radians(derece)), 5) + 250
 
     """
-    saniye = -25 * round(math.sin(math.radians(derece)), 5) + 100
-    saniye2 = -25 * round(math.cos(math.radians(derece)), 5) + 100
+    saniye  = -10 * round(math.sin(math.radians(derece)), 5) + 100
+    saniye2 = -10 * round(math.cos(math.radians(derece)), 5) + 100
     saniye3 = 25 * round(math.sin(math.radians(derece)), 5) + 100
     saniye4 = 25 * round(math.cos(math.radians(derece)), 5) + 100
     """
@@ -40,7 +53,7 @@ while run:
     xsaat   = -40 * round(math.sin(math.radians(dereceST)), 5) + 250
     ysaat   = -40 * round(math.cos(math.radians(dereceST)), 5) + 250
 
-    # pygame.draw.line(screen, (255, 255, 255), (100, 100), (saniye, saniye2))
+    # pygame.draw.line(screen, (255, 255, 255), (saniye3, saniye4), (saniye, saniye2))
     # pygame.draw.line(screen, (255, 255, 255), (100, 100), (saniye3, saniye4))
 
     pygame.draw.line(screen, (255, 255, 255), (250, 250), (xsaniye, ysaniye))
@@ -48,6 +61,12 @@ while run:
     pygame.draw.line(screen, (0, 255, 255), (250, 250), (xsaat, ysaat), 3)
 
     derece-=6
+    for each in range(360):
+        if (each) % 30 == 0:
+            xsayi = -50 * round(math.sin(math.radians(each)), 5) + 250
+            ysayi = -50 * round(math.cos(math.radians(each)), 5) + 250
+            print(f"derece:{derece}\nderece % 30:{str(int(each / 30))}")
+            screen.blit(sayi(each), (xsayi-5, ysayi-15))
 
     if derece == 0:
         derece=360
@@ -65,7 +84,13 @@ while run:
         dereceST = 360
 
     # print(f"\ndegree:{derece}\nx1:{x1 - 100}\ny1:{y1 - 100}\nx2:{x2 - 100}\ny2:{y2 - 100}\n")
-    sleep(0.1)
+    sleep_sayac+=0.001
+    if sleep_sayac == 0.001:
+        sleep_sayac = 0.01
+    sleep(sleep_sayac)
+
+
+
     """
     if y1 == 200:
         donus_y = True
